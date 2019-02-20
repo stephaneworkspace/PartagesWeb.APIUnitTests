a6cfaf89fbe7b21a32ef44000011041204d9cfd9
Bressani <Steee-Fx@hotmail.com>
2019-02-20 17:49:25 +0100

    Nouveaux unit tests

---
+++ AFaireUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PartagesWeb.APIUnitTests
+{
+    public class AFaireUnitTest
+    {
+        [Fact]
+        public void AFaireGestionPages()
+        {
+            Assert.True(1 == 0, "create TitreMenu (création UnitTest)");
+            Assert.True(1 == 0, "edit TitreMenu");
+            Assert.True(1 == 0, "effacer TitreMenu (création UnitTest)");
+            Assert.True(1 == 0, "up TitreMenu");
+            Assert.True(1 == 0, "down TitreMenu");
+        }
+    }
+}
