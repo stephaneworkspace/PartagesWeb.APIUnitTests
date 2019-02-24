cc5aaa8b2284b52363230c8392892dd68794259f
stephaneworkspace <Steee-Fx@hotmail.com>
2019-02-24 17:43:45 +0100

    mise à jour

--- AFaireUnitTest.cs
+++ AFaireUnitTest.cs
@@ -11,11 +11,17 @@ namespace PartagesWeb.APIUnitTests
         [Fact]
         public void AFaireGestionPages()
         {
+            Assert.True(1 == 0, "effacer Section quand TitreMenu (problème de relations)");
+
             Assert.True(1 == 0, "create TitreMenu (création UnitTest)");
             Assert.True(1 == 0, "edit TitreMenu");
             Assert.True(1 == 0, "effacer TitreMenu (création UnitTest)");
             Assert.True(1 == 0, "up TitreMenu");
             Assert.True(1 == 0, "down TitreMenu");
+
+            Assert.True(1 == 0, "Register erreur + seed user ne fonctionne pas en cas de delete de la database");
+
+
         }
     }
 }
