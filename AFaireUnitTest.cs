fb31f2d15b7894d377a4bb98f8854138eb5bd1b2
stephaneworkspace <Steee-Fx@hotmail.com>
2019-03-01 17:52:51 +0100

    Ajout

--- AFaireUnitTest.cs
+++ AFaireUnitTest.cs
@@ -11,13 +11,17 @@ namespace PartagesWeb.APIUnitTests
         [Fact]
         public void AFaireGestionPages()
         {
-            Assert.True(1 == 0, "effacer Section quand TitreMenu (problème de relations)");
+            // Delete, commentaire dans controller et tester
+            // En attente d'avoir tout une arborésance
 
+            // GetSections frontend bloquer les boutons pour sous titre
             Assert.True(1 == 0, "create TitreMenu (création UnitTest)");
             Assert.True(1 == 0, "edit TitreMenu");
             Assert.True(1 == 0, "effacer TitreMenu (création UnitTest)");
             Assert.True(1 == 0, "up TitreMenu");
             Assert.True(1 == 0, "down TitreMenu");
+
+            Assert.True(1 == 0, "nom unique SousTitreMenu");
 
             Assert.True(1 == 0, "Register erreur + seed user ne fonctionne pas en cas de delete de la database");
 
